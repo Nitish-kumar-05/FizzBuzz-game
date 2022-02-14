f8a7e75f7fdc65876bae1bc2a2b6c2e39e8a9e26
nitish kumar <99485371+Nitish-kumar-05@users.noreply.github.com>
2022-02-14 16:42:54 +0530

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,15 @@
+# tittle ; FizzBuzz
+
+
+for numbers in range(1, 101):
+	if numbers % 3 == 0:
+		print("Fizz")
+	elif numbers % 5 == 0:
+		print("Buzz")
+	elif numbers % 3 == 0 and numbers % 5 == 0:
+		print("FizzBuzz")
+	else:
+		print(numbers)
+		
+		
+		
