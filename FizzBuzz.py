ddc5292513b21d28c97d898b5a5f79b9d67d165e
nitish kumar <99485371+Nitish-kumar-05@users.noreply.github.com>
2022-02-14 16:45:31 +0530

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,15 +1,15 @@
 # tittle ; FizzBuzz
 
 
-for numbers in range(1, 101):
-	if numbers % 3 == 0:
-		print("Fizz")
-	elif numbers % 5 == 0:
-		print("Buzz")
-	elif numbers % 3 == 0 and numbers % 5 == 0:
-		print("FizzBuzz")
-	else:
-		print(numbers)
+for number in range(1, 101):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
 		
 		
 		
